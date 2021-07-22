60a32901a826f716454b4af0ce80116f580a1bd7
Chaitanya Santosh Tekane <chaitanyatekne5@gmail.com>
2021-07-22 10:33:29 +0530

    add SearchBox component along with css

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '../Header/Header';
+import SearchBox from '../SearchBox/SearchBox';
 import './App.css';
 
 class App extends React.Component {
@@ -12,6 +13,7 @@ class App extends React.Component {
         return (
             <div>
                 <Header headTitle={this.state.headerText} />
+                <SearchBox />
             </div>
         );
     }
