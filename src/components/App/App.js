0247b7e41affba7a43972518bfae35c8eadbe2c8
Chaitanya Santosh Tekane <chaitanyatekne5@gmail.com>
2021-07-22 10:16:14 +0530

    understanding react state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,10 +3,21 @@ import Header from '../Header/Header';
 import './App.css';
 
 class App extends React.Component {
+
+    state = {
+        headerText: 'Using setState Will Change This',
+    }
+
     render() {
         return (
             <div>
                 <Header />
+                <h1>{this.state.headerText}</h1>
+                <button onClick={() => {
+                    this.setState({
+                        headerText: 'Chaitanya Changed This',
+                    })
+                }}>Change HeaderText</button>
             </div>
         );
     }
