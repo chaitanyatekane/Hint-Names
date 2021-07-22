4cda06c250934dfb8a1a104fe61a15a1b4735262
Chaitanya Santosh Tekane <chaitanyatekne5@gmail.com>
2021-07-22 11:21:54 +0530

    add ResultsContainer component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '../Header/Header';
+import ResultsContainer from '../ResultsContainer/ResultsContainer';
 import SearchBox from '../SearchBox/SearchBox';
 import './App.css';
 
@@ -19,6 +20,7 @@ class App extends React.Component {
             <div>
                 <Header headerExpanded={this.state.headerExpanded} headTitle={this.state.headerText} />
                 <SearchBox onInputChange={this.handleInputChange} />
+                <ResultsContainer />
             </div>
         );
     }
---
+++ src/components/ResultsContainer/ResultsContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ResultsContainer.css';
+
+const ResultsContainer = () => {
+    return (
+        <div>
+            <p>These are the results!</p>
+        </div>
+    );
+};
+
+export default ResultsContainer;
